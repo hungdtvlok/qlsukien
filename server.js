b6d6f5bee44c41d112be17b4d15da8093f29d6d0
hungdtvlok <githich462@gmail.com>
2025-09-13 04:56:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true },
     avatar: { type: String, default: "" }, // thêm avatar
     createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now }
+    updatedAt: { type: Date, default: Date.now },
     sole: { type: String, default: "User" }
 });
 
@@ -276,6 +276,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`✅ Server running on http://localhost:${PORT}`);
 });
+
 
 
 
