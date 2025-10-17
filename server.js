efbdee3346fc42a0340dab70d4130b081f7ef4df
hungdtvlok <githich462@gmail.com>
2025-10-18 05:53:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ const eventSchema = new mongoose.Schema({
     location: { type: String, required: true },
     description: { type: String },
     createdAt: { type: Date, default: Date.now },
-    maxParticipants: { type: Number, default: 3 }, // số lượng tối đa
+    maxParticipants: { type: Number, default: 100 }, // số lượng tối đa
     registeredCount: { type: Number, default: 0 }    // số lượng đã đăng ký
 });
 
@@ -853,6 +853,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`✅ Server running on http://localhost:${PORT}`);
 });
+
 
 
 
