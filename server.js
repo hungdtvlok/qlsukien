75acc00813a668149ac7c51cc66846a7d7e28e1e
hungdtvlok <githich462@gmail.com>
2025-11-01 15:39:40 +0700

    Update server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ const eventSchema = new mongoose.Schema({
 
 const Event = mongoose.model("Event", eventSchema);
 const User = mongoose.model("User", userSchema);
-
+const ChiTieu = mongoose.model("ChiTieu", expenseSchema);
 
 
 // ================== API REGISTER ==================
@@ -1178,6 +1178,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`✅ Server running on http://localhost:${PORT}`);
 });
+
 
 
 
