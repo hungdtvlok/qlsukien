16848ff9b3b40859bfa37e74abdf617060a22b21
hungdtvlok <githich462@gmail.com>
2025-11-02 01:00:22 +0700

    Update server.js

--- server.js
+++ server.js
@@ -87,8 +87,8 @@ const eventSchema = new mongoose.Schema({
 
 const Event = mongoose.model("Event", eventSchema);
 const User = mongoose.model("User", userSchema);
-const ChiTieu = mongoose.model("ChiTieu", expenseSchema);
-const Chat = mongoose.model("Chat", chatSchema)
+const expenses = mongoose.model("expenses", expenseSchema);
+const chats = mongoose.model("chats", chatSchema)
 
 // ================== API REGISTER ==================
 app.post("/api/register", async (req, res) => {
@@ -1232,6 +1232,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`✅ Server running on http://localhost:${PORT}`);
 });
+
 
 
 
